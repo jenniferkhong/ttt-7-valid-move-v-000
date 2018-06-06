2f6aaa1d72afdcdecf6c4d195e8a189e2184ce6b
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-06 20:51:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if (board[index] == "X" || board [0] != "O")
+  if (board[index] == "X" || board[index] == "O")
     move = false
-  if (board[index] == "X" || board [0] != "O")
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
     move = true
   end
 end
